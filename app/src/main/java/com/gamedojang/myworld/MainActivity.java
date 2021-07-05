090fa89e8236b79fc650a3fc8c0c2bf048149d5f
LIM-BAEK-SAN <86937550+LIM-BAEK-SAN@users.noreply.github.com>
2021-07-05 14:14:33 +0900

    주석 변경

--- app/src/main/java/com/gamedojang/myworld/MainActivity.java
+++ app/src/main/java/com/gamedojang/myworld/MainActivity.java
@@ -11,6 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         
-        // 주석 테스트
+        // 주석 변경
     }
 }
